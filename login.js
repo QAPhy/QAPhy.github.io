34a2f90379ea53134241e51b871e952b7d924775
QAPhy <43513471+QAPhy@users.noreply.github.com>
2018-09-27 03:35:25 +0300

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "username1" && password == "password1"){
-alert ("Login successfully");
+   location="index/Medical_Physics_Education.html";
   }
   else{
     alert("Invalid username or password");
