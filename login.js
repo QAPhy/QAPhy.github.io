f532a9beacecdd41de07a94b1b7052fee4b82e69
QAPhy <43513471+QAPhy@users.noreply.github.com>
2018-09-27 03:42:28 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "username1" && password == "password1"){
+if ( username == "root" && password == "root"){
    location="index/Medical_Physics_Education.html";
   }
   else{
