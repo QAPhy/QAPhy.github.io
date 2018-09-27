2a5f1db929d7b076b8c4b3d16964ad003f8d4624
QAPhy <43513471+QAPhy@users.noreply.github.com>
2018-09-27 03:16:42 +0300

    Create login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "username1" && password == "password1"){
+alert ("Login successfully");
+  }
+  else{
+    alert("Invalid username or password");
+    }
+  return false;
+  }
